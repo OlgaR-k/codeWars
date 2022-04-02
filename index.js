f1aa836c96e16709f58dc7d26d51a1110a4f8830
Volha Ramanchyk <volha.ramanchyk@gmail.com>
2022-04-03 02:13:59 +0300

    feat: add solution task My Language Skills

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-function recycle(array) {
-      const typeMaterial = ['paper', 'glass', 'organic', 'plastic'];
-  
-      const rez = {
-          [typeMaterial[0]]: [],
-          [typeMaterial[1]]: [],
-          [typeMaterial[2]]: [],
-          [typeMaterial[3]]: [],
-      }
-  
-      let arrmaterial = ['material', 'secondMaterial']
-      for (let i = 0; i < array.length; i++) {
-          let obj = array[i];
-          for (let k = 0; k < arrmaterial.length; k++) {
-              if (obj[arrmaterial[k]]) {
-                  rez[obj[arrmaterial[k]]].push(obj['type'])
-              }
-          }
-      }
-  
-      let arr = [];
-      for ( let i = 0; i < typeMaterial.length; i++ ) {
-          arr.push( rez[typeMaterial[i]] );
-      }
-      return arr;
+function myLanguages(results) {
+  const MIN = 60;
+  let ball = [];
+
+  for ( let key in results ) {
+      if (results[key] >= MIN ) {
+          ball.push( results[key] );
+      }
   }
+  ball.sort( (a, b) => b - a );
+
+  let rez = Array(ball.length);
+  for ( let key in results ) {
+      if (results[key] >= MIN ) {
+         let index = ball.findIndex( item => item == results[key] );
+         rez[index] = key;
+      }
+  }
+  return rez;
+}
