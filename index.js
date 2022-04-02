4b869e396a4a0dd9380ed52a5a4741322add6b44
Volha Ramanchyk <volha.ramanchyk@gmail.com>
2022-04-02 21:01:07 +0300

    feat: task Array Deep Count

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 
-function getParticipants(handshakes) {
-  return Math.ceil(( (Math.sqrt(8*handshakes + 1) + 1) / 2));
-}
+function deepCount2(a, rez) {
+      for (let i = 0; i < a.length; i++ ) {
+          rez[0]++;
+          if ( Array.isArray(a[i]) ) {
+              deepCount2(a[i], rez);
+          }
+      }
+      return rez[0];
+  }
+  
+  function deepCount(a) {
+      let rez = [0];
+      for (let i = 0; i < a.length; i++ ) {
+          rez[0]++;
+          if ( Array.isArray(a[i]) ) {
+              deepCount2(a[i], rez);
+          }
+      }
+      return rez[0];
+  }
