637cfba8491459f71ce8e0b4227150586788eac2
Volha Ramanchyk <volha.ramanchyk@gmail.com>
2022-04-05 00:18:31 +0300

    feat: add solution task Unlucky Days

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
-      if (enteredCode !== correctCode) {
-        return false;
-      }
-      if ((new Date(currentDate)) > (new Date(expirationDate))) {
-        return false;
-      }
-      return true;
+function unluckyDays(year){
+    let rez = 0;
+    for ( let y = 0; y <= 11; y++ ) {
+        if ( ( new Date(year, y, 13)).getDay() == 5 ) {
+            rez++;
+        }
     }
+    return rez;
+}
+
+
+  console.log(
+    unluckyDays(2015)
+  );
