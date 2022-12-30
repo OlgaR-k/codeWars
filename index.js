2c4d2fd11d6a55e17e3fae0006cabd611377808a
Olga Romanchik <volha.ramanchyk@gmail.com>
2022-12-31 01:07:42 +0300

    feat: task Data Reverse

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function launchAll(launchMissile) {
-    for(let i = 0; i < 5; i++) {
-      setTimeout(function() {
-        launchMissile(i);
-      }, i * 1000);
-    }
-  }
+function dataReverse(data) {
+  let rez = [];
+
+  while (data.length > 0) {
+       rez.unshift(...data.splice(0, 8));
+   }
+   console.log(rez);
+  return rez;
+
+}
+
+dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]);
