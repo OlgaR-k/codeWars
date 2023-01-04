2381688bc46cd2789bec39664b1ed8400ebee9b9
Olga Romanchik <volha.ramanchyk@gmail.com>
2023-01-04 13:38:38 +0300

    feat: Count the Digit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 
-function spread(func, args) {
-  console.log(...args);
-  return func(...args); 
+function nbDig(n, d) {
+  let rez = [];
+
+  for ( let i = 0; i <= n; i++ ) {
+    rez.push( Math.pow(i, 2) );
+  }
+  let count = 0;
+  for ( let i = 0; i <= rez.length; i++ ) {
+    String(rez[i]).split('').forEach( (item) => {
+      if (item == d) {
+        ++count;
+      } 
+    });
+    
+  }
+  return count;
 }
 
-console.log( spread(function(x,y){return x+y}, [1,2]) );
+console.log( nbDig(10, 1) );
