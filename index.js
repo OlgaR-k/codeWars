9231455073a7e2f63fc81930226e2c143f456a6e
Olga Romanchik <volha.ramanchyk@gmail.com>
2023-01-04 13:03:19 +0300

    feat: task Unpacking Arguments

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 
-function sentence(List) {
-  let order = [];
-  List.forEach( (item) => {
-      order.push( Object.entries(item)[0][0] );
-  } );
-  order.sort( (a, b) => a - b );
-
-  let result = new Array(order.length);
-
-  List.forEach( (item) => {
-    let [key, value] = [Object.keys(item)[0], Object.values(item)[0]];
-    result[order.findIndex( val => val == key)] = value;
-  } );
-
-  return result.join(' ');
+function spread(func, args) {
+  console.log(...args);
+  return func(...args); 
 }
 
-const List = [
-  {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
-  {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
- ];
-
-console.log(sentence(List));
+console.log( spread(function(x,y){return x+y}, [1,2]) );
